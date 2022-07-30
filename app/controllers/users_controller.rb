8129114e6cdec8b1b6e847984b781a2bf46477ee
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-07-30 15:25:06 +0000

    [Bresk] Bookers2-debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
+    @user = User.find(params[:id])
+    unless @user == current_user
+    redirect_to user_path(current_user)
+    end
   end
 
   def update
